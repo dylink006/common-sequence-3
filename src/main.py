19fdcf799e2ff3d38e492bc37cf25bd2868f6edb
dylmcgarry <dylan.t.mcgarry@gmail.com>
2026-04-02 14:55:51 -0400

    separate file for hvlcs algorithm made and imported into main. Main now takes in file as input and does thorough checks to make sure file is formatted correctly

--- src/main.py
+++ src/main.py
@@ -1,43 +1,70 @@
-def parse_input():
-    k = 3
-    values = {'a':2, 'b':4, 'c':5}
-    a = 'aacb'
-    b = 'caab'
-    return (values, a, b)
+from hvlcs import hvlcs
+import sys
 
-def hvlcs(s1:str, s2:str, values:dict[str, int]):
-    n = len(s1)
-    m = len(s2)
-    dp = [[0] * (m + 1) for i in range(n + 1)]
+def main(filepath):
+    try:
+        with open(filepath, 'r') as f:
+            line1 = f.readline().split()
+            if not line1:
+                raise ValueError("Missing k")
+            if len(line1) != 1:
+                raise ValueError("First line must contain only integer k")
+            k = line1[0]
+            if not k.isdigit():
+                raise ValueError("k must be an integer")
+            k = int(k)
 
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            if s1[i - 1] == s2[j - 1]:
-                dp[i][j] = values[s1[i - 1]] + dp[i - 1][j - 1]
-            else:
-                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
-    max_val = dp[n][m]
-
-    chars = []
-    while n > 0 and m > 0:
-        if s1[n - 1] == s2[m - 1] and dp[n][m] == dp[n - 1][m - 1] + values[s1[n - 1]]:
-            chars.append(s1[n - 1])
-            n -= 1
-            m -= 1
-        elif dp[n - 1][m] >= dp[n][m - 1]:
-            n -= 1
-        else:
-            m -= 1
-    chars.reverse()
-
-    return max_val, "".join(chars)
-
-def main():
-    values, a, b = parse_input()
-    val, substr = hvlcs(a, b, values)
-    print(val)
-    print(substr)
+            values = {}
+            for i in range(k):
+                line = f.readline().split()
+                if not line:
+                    raise ValueError(f"Missing value line {i + 1}")
+                if len(line) != 2:
+                    raise ValueError(f"Value line {i + 1} must contain a character and an integer")
+                c = line[0]
+                n = line[1]
+                if len(c) != 1:
+                    raise ValueError(f"Key on line {i + 1} must be a single character")
+                if not n.isdigit():
+                    raise ValueError(f"Value on line {i + 1} must be a positive integer")
+                values[c.lower()] = int(n)
+            
+            string1line = f.readline().split()
+            if not string1line:
+                raise ValueError("Missing first string")
+            if len(string1line) != 1:
+                raise ValueError("First string line must contain only one string")
+            string1 = string1line[0]
+            if not string1.isalpha():
+                raise ValueError("First string contains non-alphabetic characters")
+            string1 = string1.lower()
+            if any(char not in values for char in string1):
+                raise ValueError("First string contains characters not in values dictionary")
+            
+            string2line = f.readline().split()
+            if not string2line:
+                raise ValueError("Missing second string")
+            if len(string2line) != 1:
+                raise ValueError("Second string line must contain only one string")
+            string2 = string2line[0]
+            if not string2.isalpha():
+                raise ValueError("Second string contains non-alphabetic characters")
+            string2 = string2.lower()
+            if any(char not in values for char in string1):
+                raise ValueError("Second string contains characters not in values dictionary")
+            
+            val, seq = hvlcs(string1, string2, values)
+            print(val)
+            print(seq)
+    except FileNotFoundError:
+        print("Bad file path, file does not exist.")
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            main(arg)
+    else:
+        print("No filepath given. Usage: python3 main.py <filename1> <optional filenames...>")
